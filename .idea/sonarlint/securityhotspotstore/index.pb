510e4ef9487725dc97bab1da561dcd8c64e9fe27
shashanka.hegade <shashanka.hegade@westernacher.com>
2024-01-21 17:36:48 +0530

    eureka server with new springboot

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+F
+Ecommerce/build.gradle,c\e\ce42a7cecd232d2ed7a7867c7661d272acdd575b
+w
+GEcommerce/src/main/java/com/example/ecommerce/EcommerceApplication.java,c\a\ca1b59c4b74f5de5f7b1f5cd1a57a7f89202e073
+F
+eureka/settings.gradle,3\a\3a4511cf38f42bcca4b98d3cfa20b841f5796941
+I
+Ecommerce/settings.gradle,f\c\fcd529a3a93a8ac8ecea4d8c444e4d8a82610fc0
+G
+Ecommerce/Ecommerce.iml,7\4\749e8e66c5e045804304cf300c18871ac0787724
---
+++ payment-gateway/src/main/java/com/example/paymentgateway/PaymentGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.paymentgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentGatewayApplication.class, args);
+  }
+
+}
